261ba41b12c8cae57a9b18c3f5603e62031102b3
Klaus Löffelmann <klaus.loeffelmann@microsoft.com>
2026-05-30 22:07:12 -0700

    Fixed merge issue.

--- src/WinForms/NET10/WinBaas/WinBaas.App/Services/ConsoleLoggerSink.cs
+++ src/WinForms/NET10/WinBaas/WinBaas.App/Services/ConsoleLoggerSink.cs
@@ -1,6 +1,6 @@
 using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
-using WarpToolkit.WinForms.Tooling;
+using WarpToolkit.WinForms.Specialized;
 
 namespace WinBaas.Services;
 
